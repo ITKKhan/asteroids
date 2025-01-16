48cd9c9f0c0b33bfcbdde5941eff861cadd447d0
ITKKhan <aargmn@gmail.com>
2025-01-15 21:43:50 -0500

    Update fps for asteroids

--- main.py
+++ main.py
@@ -11,12 +11,17 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    ticks = pygame.time.Clock()
+    dt = 0 
+
     while True: 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return
+            if event.type == pygame.QUIT:                
+                return 
         screen.fill((0,0,0))
         pygame.display.flip()
+        dt = ticks.tick(60)
+        dt /= 1000  
 
 if __name__ == "__main__":
         main()
